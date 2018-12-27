c700f127786a083bc519d1b00937a7797386a49a
aatack <alexanderjtatack@gmail.com>
2018-12-27 01:05:22 +0000

    Convert pointers to use safe interface

--- Optimall/Definition/Pointer.hs
+++ Optimall/Definition/Pointer.hs
@@ -3,23 +3,28 @@ module Optimall.Definition.Pointer
 , resolve
 , update
 , (>:>)
-, (>::>)
+, stackPointers
+, keyPointer
+, indexPointer
 ) where
 
 import qualified Data.Map as Map
 import Optimall.Definition.Hierarchy
 
 -- | Contains functions for referencing and updating hierarchies.
-data Pointer a b = Pointer (Hierarchy a b -> Hierarchy a b)
+data Pointer a b = Pointer (Hierarchy a b -> Maybe (Hierarchy a b))
     ((Hierarchy a b -> Hierarchy a b)
         -> Hierarchy a b -> Hierarchy a b)
 
--- | Extract the subhierarchy referenced by a pointer.
-resolve :: Pointer a b -> Hierarchy a b -> Hierarchy a b
+-- | Extract the subhierarchy referenced by a pointer.  If
+-- the subhierarchy does not exist, gives Nothing.
+resolve :: Pointer a b -> Hierarchy a b -> Maybe (Hierarchy a b)
 resolve (Pointer r _) = r
 
 -- | Update the value stored in the location referenced by
--- a pointer according to some transfer function.
+-- a pointer according to some transfer function.  If the
+-- subhierarchy does not exist, this will simply give back
+-- the input hierarchy, unaltered.
 update :: Pointer a b -> (Hierarchy a b -> Hierarchy a b)
     -> Hierarchy a b -> Hierarchy a b
 update (Pointer _ u) = u
@@ -28,22 +33,24 @@ update (Pointer _ u) = u
 (>:>) :: Pointer a b -> Pointer a b -> Pointer a b
 (>:>) (Pointer r u) (Pointer r' u') = Pointer r'' u''
     where
-        r'' = r' . r
+        r'' h = case r h of
+            Just h' -> r' h'
+            Nothing -> Nothing
         u'' f = u (u' f)
 
 -- | Compose a list of pointers.
-(>::>) :: [Pointer a b] -> Pointer a b
-(>::>) = foldr1 (>:>)
+stackPointers :: [Pointer a b] -> Pointer a b
+stackPointers = foldr1 (>:>)
 
 -- | Create a pointer that accesses a certain key
 -- of a keyed hierarchy.
 keyPointer :: String -> Pointer a b
 keyPointer k = Pointer r u
     where
-        r (Keyed m _) = m Map.! k
-        r _ = error ""
+        r (Keyed m _) = Map.lookup k m
+        r _ = Nothing
         u f (Keyed m md) = Keyed (Map.adjust f k m) md
-        u _ _ = error ""
+        u _ h = h
 
 -- | Create a pointer that accesses a certain element
 -- within an ordered hierarchy.  This is slow and may
@@ -51,9 +58,11 @@ keyPointer k = Pointer r u
 indexPointer :: Int -> Pointer a b
 indexPointer i = Pointer r u
     where
-        r (Ordered l _) = l !! i
-        r _ = error ""
+        r (Ordered l _)
+            | length l > i = Just $ l !! i
+            | otherwise    = Nothing
+        r _ = Nothing
         u f (Ordered l md) =
             let l' = (take i l ++ [f $ l !! i] ++ drop (i + 1) l)
             in Ordered l' md
-        u _ _ = error ""
+        u _ h = h
